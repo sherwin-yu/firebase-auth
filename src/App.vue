222d4fa0c34fdf44b84cb6299759225133ad26c1
Sherwin Yu <sherwinhyu@gmail.com>
2020-05-16 02:06:48 -0400

    styling of form

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,14 @@
 </template>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
 }
 
